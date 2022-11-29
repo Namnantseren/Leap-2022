df4c35a832a917305a30128e8098e81d29f5170b
Namnantseren <namka1472@gmail.com>
2022-11-29 23:15:02 +0800

    Killing time

---
+++ js/Homework/Huvisagch.js
@@ -0,0 +1,22 @@
+let a = 85, b = 75, c = 96, d = 69;
+
+
+switch (a + b) {
+    case (+b):
+        console.log("160")
+        break;
+    case (+c):
+        console.log("181")
+        break;
+    case (+d):
+        console.log("154")
+        break;
+    default:
+        console.log("Error")
+} 
+
+// let a1 = 3, b1 = 7, c1 = 2, d1 = 4;
+
+// if(a1 * b1 * c1) {
+//     // console.log("5-аас бага тоонуудын үржвэр: 24")
+// }
