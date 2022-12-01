ae1168a81c52a67564fbc6011f9573fc5e926703
Morning <morning@admins-iMac.local>
2022-12-01 12:38:32 +0800

    Remaining

--- js/Condition/first.js
+++ js/Condition/first.js
@@ -153,3 +153,34 @@
 
 // console.log(Math.floor(Math.random() * (Math.abs(a - b) + 1)) + Math.min(a, b)); 
 
+
+
+//============  =======    Loop   =======  ================  //
+
+
+// let count = 10;
+// while (count >= 1) {
+//     console.log(count);
+//     count = count - 1;
+// }
+
+let count = 0;
+let countA = 0;
+let lorem = "What is Lorem Ipsum Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+
+
+while (count < lorem.length) {
+    if (lorem[count] == "a" || lorem[countA] == "A") {
+        countA = countA + 1;
+    }
+    count = count + 1;
+}
+
+console.log(count);
+
+
+
+
+
+// ================= =======    Loop   =======   ===================== //
+
---
+++ js/basic/loop.js
@@ -0,0 +1,63 @@
+//============  =======    Loop-1   =======  ================  //
+
+
+// let count = 10;
+// while (count >= 1) {
+//     console.log(count);
+//     count = count - 1;
+// }
+
+// let count = 0;
+// let countA = 0;
+// let lorem = "What is Lorem Ipsum Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+
+
+// while (count < lorem.length) {
+//     if (lorem[count] == "a" || lorem[countA] == "A") {
+//         count = countA + 1;
+//     }
+//     count = count + 1;
+// }
+
+// console.log(count);
+
+// =============================== Loop-2 ====================================== //
+
+let number = 100;
+let count1 = 0;
+
+while (count1 < number) {
+    
+    if (number % 3 === count1) {
+        // count1++;
+    }
+
+    // count1--;
+}
+
+console.log(count1);
+
+
+// ================================ Loop-3 =============================== //
+
+
+// let x = 10;
+// let count = 1;
+// let output = "";
+
+// while (x > 0) {
+//     let y = 10;
+//     while (y > 0) {
+//         output = output + count + "\t";
+//         if (count % 10 == 0) output+= "\n" //output = output + "\n"
+//         y--;
+//         count++;
+//     }
+//     x--;
+// }
+
+// console.log(output);
+
+
+// ================= =======    Loop   =======   ===================== //
+
