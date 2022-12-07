4bc18ef2c394dc6202ff20ad5ef0af3dd8d57ef8
Morning <morning@admins-iMac.local>
2022-12-07 12:34:09 +0800

    Array method

--- js/basic/function.js
+++ js/basic/function.js
@@ -73,22 +73,22 @@
 
 //====================================== Let function=============================== //
 
-let input = "HELLO WORLD";
-const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const lower = "abcdefghijklmnopqrstuvwxyz";
+// let input = "HELLO WORLD";
+// const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// const lower = "abcdefghijklmnopqrstuvwxyz";
  
-function converter(str) {
-    let result = "";
-    for (i = 0; i < str.lenght; i++) {
-        if (upper.includes(str[i])) {
-            result += str[i].toLowerCase();
-        } else if (lower.includes(str[i])) {
-            result += str[i].toUpperCase();
-        } else {
-            result += str[i];
-        }
-    }
-    return result;
-}
+// function converter(str) {
+//     let result = "";
+//     for (i = 0; i < str.lenght; i++) {
+//         if (upper.includes(str[i])) {
+//             result += str[i].toLowerCase();
+//         } else if (lower.includes(str[i])) {
+//             result += str[i].toUpperCase();
+//         } else {
+//             result += str[i];
+//         }
+//     }
+//     return result;
+// }
 
-console.log(converter("HELLO"));
+// console.log(converter("HELLO"));
