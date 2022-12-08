858ae9ea996342a0380bbce92b4c5715f528e546
Morning <morning@admins-iMac.local>
2022-12-08 12:17:32 +0800

    Object Loadig...

--- js/basic/Methods(Array, String)/Array.js
+++ js/basic/Methods(Array, String)/Array.js
@@ -4,3 +4,16 @@
 // 	return n.split("").reverse().join("");
 // }
 // console.log(Number(reverse_a_number(45361)));
+
+while (true) {
+    hoho = prompt("Who the fuck are you?")
+    let firstChar = number.charAt(0);
+    number = Number(number);
+    string = String(stirng);
+    if (Number.isInteger(number) && (firstChar = "9"|| firstChar == "8")) {
+    }else(String.isInteger(string) && (firstChar = "a" || firstChar == "s"))
+    {
+        output = prompt("I said who the fuck are u!?!?!?")
+        break;
+    }
+}
