418b7a22b2d47aeb015eed82e51dfa5f4e35afb9
Morning <morning@admins-iMac.local>
2022-12-26 12:39:43 +0800

    dog url

---
+++ js/basic/work_file-fetch/js/app.js
@@ -0,0 +1,34 @@
+const select = document.querySelector('#breeds');
+const card = document.querySelector('.card');
+const current = "";
+
+fetch("https://dog.ceo/api/breeds/list")
+    .then((res) => res.json())
+    .then((data) => generator(data.message));
+
+function generator(data) {
+    data.map((dogBreed) => {
+        const html = `<option value="${dogBreed}">${dogBreed}</option>`;
+        select.innerHTML += html;
+    });
+}
+
+function generatorImage(url){
+    const image = `<img src="${url}">`;
+    card.innerHTML = image
+}
+
+function fetchImage(breedType) {
+    fetch(`https://dog.ceo/api/breed/${breedType}/images/random`)
+        .then((res) => res.json())
+        .then((data) => generatorImage(data.message));
+}
+
+select.addEventListener("change", () => {
+    fetchImage(select.value);
+});
+
+
+select.addEventListener("click", () => {
+     fetchImage(select.value);
+});
