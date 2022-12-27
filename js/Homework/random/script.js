589010cd66be0d4aacb2f4a9ab9f7313a0f25712
Namnantseren <namka1472@gmail.com>
2022-12-27 22:15:39 +0800

    practice

---
+++ js/Homework/random/script.js
@@ -0,0 +1,29 @@
+// let input = 10;
+
+// if(input < 10) {
+//     console.log(input);
+// }else {
+//     console.log(false);
+// }
+
+// let hoho = "bob you so fat chicken"
+
+// console.log(hoho.indexOf("so"))
+
+
+// function baldan(x){
+//     if(Number.isInteger(x)) {
+//         return "true"
+//     }else{
+//         return "false"
+//     }
+// }
+
+// console.log(baldan(5.1))
+
+// let tsombo = Math.random();
+
+let high = Math.ceil(Math.random());
+
+
+console.log(high);
